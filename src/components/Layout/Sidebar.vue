13d6cfcb94ed2d734a1d1c24455b6fd1b0262e9e
Hamed <USER@DESKTOP-20DRNS1>
2024-07-04 21:45:39 +0330

    resulve conflict

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -2,26 +2,26 @@
     <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
     <aside class="app-sidebar sidebar-scroll">
         <div class="main-sidebar-header active">
-            <a class="desktop-logo logo-light active" href="/dashboard"><img :src="logoPath" style="width: 45px !important;height: 45px !important;"
-                    class="main-logo" alt="logo">
-                 </a>
-            <a class="desktop-logo logo-dark active" href="/dashboard"><img :src="logoPath"
-                    class="main-logo dark-theme" alt="logo"></a>
-            <a class="logo-icon mobile-logo icon-light active" href="/dashboard"><img
-                    :src="logoPath" class="logo-icon" alt="logo"></a>
+            <a class="desktop-logo logo-light active" href="/dashboard"><img :src="logoPath"
+                    style="width: 45px !important;height: 45px !important;" class="main-logo" alt="logo">
+            </a>
+            <a class="desktop-logo logo-dark active" href="/dashboard"><img :src="logoPath" class="main-logo dark-theme"
+                    alt="logo"></a>
+            <a class="logo-icon mobile-logo icon-light active" href="/dashboard"><img :src="logoPath" class="logo-icon"
+                    alt="logo"></a>
             <a class="logo-icon mobile-logo icon-dark active" href="/dashboard">
                 <img :src="logoPath" class="logo-icon dark-theme" alt="logo"></a>
         </div>
         <div class="main-sidemenu">
-            <h6 class="center darktext">   {{ organizationPersianTitleName }}</h6>
+            <h6 class="center darktext"> {{ organizationPersianTitleName }}</h6>
             <div class="app-sidebar__user clearfix">
                 <div class="dropdown user-pro-body">
 
                     <div class="">
                         <router-link :to="{ name: 'profile' }">
 
-                         <img alt="user-img" class="avatar avatar-xl brround" :src="avatarPath"><span
-                            class="avatar-status profile-status bg-green"></span>
+                            <img alt="user-img" class="avatar avatar-xl brround" :src="avatarPath"><span
+                                class="avatar-status profile-status bg-green"></span>
                         </router-link>
 
                     </div>
@@ -36,39 +36,79 @@
             <ul class="side-menu">
                 <li class="slide">
                     <router-link :to="{ name: 'dashboard' }" class="side-menu__item">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z" opacity=".3"></path><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path></svg>                        <span class="side-menu__label">داشبورد</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z" opacity=".3"></path>
+                            <path
+                                d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z">
+                            </path>
+                        </svg> <span class="side-menu__label">داشبورد</span>
                     </router-link>
                 </li>
- 
+
 
                 <li class="slide">
                     <a class="side-menu__item" data-bs-toggle="slide" href="#">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" class="side-menu__icon" viewBox="0 0 24 24"><g><rect fill="none"></rect></g><g><g></g><g><path d="M21,5c-1.11-0.35-2.33-0.5-3.5-0.5c-1.95,0-4.05,0.4-5.5,1.5c-1.45-1.1-3.55-1.5-5.5-1.5S2.45,4.9,1,6v14.65 c0,0.25,0.25,0.5,0.5,0.5c0.1,0,0.15-0.05,0.25-0.05C3.1,20.45,5.05,20,6.5,20c1.95,0,4.05,0.4,5.5,1.5c1.35-0.85,3.8-1.5,5.5-1.5 c1.65,0,3.35,0.3,4.75,1.05c0.1,0.05,0.15,0.05,0.25,0.05c0.25,0,0.5-0.25,0.5-0.5V6C22.4,5.55,21.75,5.25,21,5z M3,18.5V7 c1.1-0.35,2.3-0.5,3.5-0.5c1.34,0,3.13,0.41,4.5,0.99v11.5C9.63,18.41,7.84,18,6.5,18C5.3,18,4.1,18.15,3,18.5z M21,18.5 c-1.1-0.35-2.3-0.5-3.5-0.5c-1.34,0-3.13,0.41-4.5,0.99V7.49c1.37-0.59,3.16-0.99,4.5-0.99c1.2,0,2.4,0.15,3.5,0.5V18.5z"></path><path d="M11,7.49C9.63,6.91,7.84,6.5,6.5,6.5C5.3,6.5,4.1,6.65,3,7v11.5C4.1,18.15,5.3,18,6.5,18 c1.34,0,3.13,0.41,4.5,0.99V7.49z" opacity=".3"></path></g><g><path d="M17.5,10.5c0.88,0,1.73,0.09,2.5,0.26V9.24C19.21,9.09,18.36,9,17.5,9c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,10.69,16.18,10.5,17.5,10.5z"></path><path d="M17.5,13.16c0.88,0,1.73,0.09,2.5,0.26V11.9c-0.79-0.15-1.64-0.24-2.5-0.24c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,13.36,16.18,13.16,17.5,13.16z"></path><path d="M17.5,15.83c0.88,0,1.73,0.09,2.5,0.26v-1.52c-0.79-0.15-1.64-0.24-2.5-0.24c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,16.02,16.18,15.83,17.5,15.83z"></path></g></g></svg>                        <span class="side-menu__label">اطلاعات شخصی</span><i
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"
+                            class="side-menu__icon" viewBox="0 0 24 24">
+                            <g>
+                                <rect fill="none"></rect>
+                            </g>
+                            <g>
+                                <g></g>
+                                <g>
+                                    <path
+                                        d="M21,5c-1.11-0.35-2.33-0.5-3.5-0.5c-1.95,0-4.05,0.4-5.5,1.5c-1.45-1.1-3.55-1.5-5.5-1.5S2.45,4.9,1,6v14.65 c0,0.25,0.25,0.5,0.5,0.5c0.1,0,0.15-0.05,0.25-0.05C3.1,20.45,5.05,20,6.5,20c1.95,0,4.05,0.4,5.5,1.5c1.35-0.85,3.8-1.5,5.5-1.5 c1.65,0,3.35,0.3,4.75,1.05c0.1,0.05,0.15,0.05,0.25,0.05c0.25,0,0.5-0.25,0.5-0.5V6C22.4,5.55,21.75,5.25,21,5z M3,18.5V7 c1.1-0.35,2.3-0.5,3.5-0.5c1.34,0,3.13,0.41,4.5,0.99v11.5C9.63,18.41,7.84,18,6.5,18C5.3,18,4.1,18.15,3,18.5z M21,18.5 c-1.1-0.35-2.3-0.5-3.5-0.5c-1.34,0-3.13,0.41-4.5,0.99V7.49c1.37-0.59,3.16-0.99,4.5-0.99c1.2,0,2.4,0.15,3.5,0.5V18.5z">
+                                    </path>
+                                    <path
+                                        d="M11,7.49C9.63,6.91,7.84,6.5,6.5,6.5C5.3,6.5,4.1,6.65,3,7v11.5C4.1,18.15,5.3,18,6.5,18 c1.34,0,3.13,0.41,4.5,0.99V7.49z"
+                                        opacity=".3"></path>
+                                </g>
+                                <g>
+                                    <path
+                                        d="M17.5,10.5c0.88,0,1.73,0.09,2.5,0.26V9.24C19.21,9.09,18.36,9,17.5,9c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,10.69,16.18,10.5,17.5,10.5z">
+                                    </path>
+                                    <path
+                                        d="M17.5,13.16c0.88,0,1.73,0.09,2.5,0.26V11.9c-0.79-0.15-1.64-0.24-2.5-0.24c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,13.36,16.18,13.16,17.5,13.16z">
+                                    </path>
+                                    <path
+                                        d="M17.5,15.83c0.88,0,1.73,0.09,2.5,0.26v-1.52c-0.79-0.15-1.64-0.24-2.5-0.24c-1.28,0-2.46,0.16-3.5,0.47v1.57 C14.99,16.02,16.18,15.83,17.5,15.83z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg> <span class="side-menu__label">اطلاعات شخصی</span><i
                             class="angle fe fe-chevron-down"></i></a>
                     <ul class="slide-menu">
-                             <router-link :to="{ name: 'profile' }" class="slide-item">
-                                پروفایل من
-                            </router-link>
-                     </ul>
+                        <router-link :to="{ name: 'profile' }" class="slide-item">
+                            پروفایل من
+                        </router-link>
+                    </ul>
                 </li>
 
 
                 <li class="slide">
                     <a class="side-menu__item" data-bs-toggle="slide" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M5 5h4v4H5zm10 10h4v4h-4zM5 15h4v4H5zM16.66 4.52l-2.83 2.82 2.83 2.83 2.83-2.83z" opacity=".3"></path><path d="M16.66 1.69L11 7.34 16.66 13l5.66-5.66-5.66-5.65zm-2.83 5.65l2.83-2.83 2.83 2.83-2.83 2.83-2.83-2.83zM3 3v8h8V3H3zm6 6H5V5h4v4zM3 21h8v-8H3v8zm2-6h4v4H5v-4zm8-2v8h8v-8h-8zm6 6h-4v-4h4v4z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path d="M5 5h4v4H5zm10 10h4v4h-4zM5 15h4v4H5zM16.66 4.52l-2.83 2.82 2.83 2.83 2.83-2.83z"
+                                opacity=".3"></path>
+                            <path
+                                d="M16.66 1.69L11 7.34 16.66 13l5.66-5.66-5.66-5.65zm-2.83 5.65l2.83-2.83 2.83 2.83-2.83 2.83-2.83-2.83zM3 3v8h8V3H3zm6 6H5V5h4v4zM3 21h8v-8H3v8zm2-6h4v4H5v-4zm8-2v8h8v-8h-8zm6 6h-4v-4h4v4z">
+                            </path>
+                        </svg>
 
-                        <span class="side-menu__label">اطلاعات پایه</span><i
-                            class="angle fe fe-chevron-down"></i></a>
+                        <span class="side-menu__label">اطلاعات پایه</span><i class="angle fe fe-chevron-down"></i></a>
                     <ul class="slide-menu">
                         <router-link :to="{ name: 'grade' }" class="slide-item"> پایه تحصیلی </router-link>
                         <router-link :to="{ name: 'organization' }" class="slide-item"> سازمان </router-link>
-                        <router-link :to="{ name: 'chart' }" class="slide-item"> چارت سازمانی </router-link>
+                        <router-link :to="{ name: 'charts_index' }" class="slide-item"> چارت سازمانی </router-link>
                         <router-link :to="{ name: 'knowledgefield' }" class="slide-item"> فیلد دانش </router-link>
-                        <router-link :to="{ name: 'homepagesetting' }" class="slide-item"> تنظیمات صفحه نخست   </router-link>
+                        <router-link :to="{ name: 'homepagesetting' }" class="slide-item"> تنظیمات صفحه نخست
+                        </router-link>
                         <router-link :to="{ name: 'formpage' }" class="slide-item"> فرم ها  </router-link>
 
-                        
+
                     </ul>
                 </li>
 
--- src/router.js
+++ src/router.js
@@ -1,121 +1,122 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { isAuth } from '@/services/HelperService'
 const routes = [{
-        path: '/',
-        component: () => { return import ('./components/Layout/Master.vue') },
-        name: 'master',
-        meta: {
-            authRequired: true
-        },
-        children: [{
-                path: 'dashboard',
-                name: 'dashboard',
-                component: () => { return import ('./components/Dashboard/Dashboard.vue') }
-            }, {
-                path: 'profile',
-                name: 'profile',
-                component: () => { return import ('./components/Profile/Profile.vue') }
-            },
-
-            {
-                path: 'grade',
-                name: 'grade',
-                component: () => { return import ('./components/Grade/GradePage.vue') }
-            },
-            {
-                path: 'organization',
-                name: 'organization',
-                component: () => { return import ('./components/Organization/OrganizationPage.vue') }
-            },
-            {
-                path: 'organization/create',
-                name: 'create_organization',
-                component: () => { return import ('./components/Organization/OrganizationCreate.vue') }
-            },
-            {
-                path: 'organization/:id/edit',
-                name: 'edit_organization',
-                component: () => { return import ('./components/Organization/OrganizationEdit.vue') }
-            },
-            {
-                path: 'chart',
-                name: 'chart',
-                component: () => { return import ('./components/Chart/ChartPage.vue') }
-            },
-            {
-                path: 'chart/create',
-                name: 'create_chart',
-                component: () => { return import ('./components/Chart/ChartCreate.vue') }
-            },
-            {
-                path: 'chart/:id/edit',
-                name: 'edit_chart',
-                component: () => { return import ('./components/Chart/ChartEdit.vue') }
-            },
-            {
-                path: 'knowledgefield',
-                name: 'knowledgefield',
-                component: () => { return import ('./components/KnowledgeField/KnowledgeFieldPage.vue') }
-            },
-            {
-                path: 'knowledgefield/create',
-                name: 'create_knowledgefield',
-                component: () => { return import ('./components/KnowledgeField/KnowledgeFieldCreate.vue') }
-            },
-            {
-                path: 'knowledgefield/:id/edit',
-                name: 'edit_knowledgefield',
-                component: () => { return import ('./components/KnowledgeField/KnowledgeFieldEdit.vue') }
-            },
-            {
-                path: 'homepagesetting',
-                name: 'homepagesetting',
-                component: () => { return import('./components/HomePageSetting/HomePageSetting.vue') }
-            },
-            {
-                path: 'formpage',
-                name: 'formpage',
-                component: () => { return import('./components/Form/FormPage.vue') }
-            },
-            {
-                path: 'formcreate',
-                name: 'formcreate',
-                component: () => { return import('./components/Form/FormCreate.vue') }
-            }
-        ],
-        beforeEnter: (to, from, next) => {
-            if (isAuth()) {
-                next()
-            }
-            next({ name: 'login' })
-        }
+    path: '/',
+    component: () => { return import('./components/Layout/Master.vue') },
+    name: 'master',
+    meta: {
+        authRequired: true
     },
-    {
-        path: '/:pathMatch(.*)*',
-        component: () => { return import ('./components/NotFound/NotFound.vue') },
-        name: 'notfound'
+    children: [{
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => { return import('./components/Dashboard/Dashboard.vue') }
     }, {
-        path: '/login',
-        component: () => { return import ('./components/Login/Login.vue') },
-        name: 'login',
-        beforeEnter: (to, from, next) => {
-            if (!isAuth()) {
-                next()
-            }
-            next({ name: 'dashboard' })
-        }
+        path: 'profile',
+        name: 'profile',
+        component: () => { return import('./components/Profile/Profile.vue') }
+    },
+
+    {
+        path: 'grade',
+        name: 'grade',
+        component: () => { return import('./components/Grade/GradePage.vue') }
     },
     {
-        path: '/register',
-        component: () => { return import ('./components/Register/Register.vue') },
-        name: 'register',
-        beforeEnter: (to, from, next) => {
-            if (!isAuth()) {
-                next()
-            }
-            next({ name: 'dashboard' })
-        }
+        path: 'organization',
+        name: 'organization',
+        component: () => { return import('./components/Organization/OrganizationPage.vue') }
+    },
+    {
+        path: 'organization/create',
+        name: 'create_organization',
+        component: () => { return import('./components/Organization/OrganizationCreate.vue') }
+    },
+    {
+        path: 'organization/:id/edit',
+        name: 'edit_organization',
+        component: () => { return import('./components/Organization/OrganizationEdit.vue') }
+    },
+    {
+        path: 'charts',
+        name: 'charts_index',
+        component: () => { return import('./components/Chart/ChartPage.vue') }
+    },
+    {
+        path: 'charts/create',
+        name: 'create_charts',
+        component: () => { return import('./components/Chart/ChartCreate.vue') }
+    },
+    {
+        path: 'charts/:id/edit',
+        name: 'edit_charts',
+        component: () => { return import('./components/Chart/ChartEdit.vue') }
+    },
+    {
+        path: 'knowledgefield',
+        name: 'knowledgefield',
+        component: () => { return import('./components/KnowledgeField/KnowledgeFieldPage.vue') }
+    },
+    {
+        path: 'knowledgefield/create',
+        name: 'create_knowledgefield',
+        component: () => { return import('./components/KnowledgeField/KnowledgeFieldCreate.vue') }
+    },
+    {
+        path: 'knowledgefield/:id/edit',
+        name: 'edit_knowledgefield',
+        component: () => { return import('./components/KnowledgeField/KnowledgeFieldEdit.vue') }
+    },
+    {
+        path: 'homepagesetting',
+        name: 'homepagesetting',
+        component: () => { return import('./components/HomePageSetting/HomePageSetting.vue') }
+    },
+    {
+        path: 'formpage',
+        name: 'formpage',
+        component: () => { return import('./components/Form/FormPage.vue') }
+    },
+    {
+        path: 'formcreate',
+        name: 'formcreate',
+        component: () => { return import('./components/Form/FormCreate.vue') }
     }
+    
+    ],
+    beforeEnter: (to, from, next) => {
+        if (isAuth()) {
+            next()
+        }
+        next({ name: 'login' })
+    }
+},
+{
+    path: '/:pathMatch(.*)*',
+    component: () => { return import('./components/NotFound/NotFound.vue') },
+    name: 'notfound'
+}, {
+    path: '/login',
+    component: () => { return import('./components/Login/Login.vue') },
+    name: 'login',
+    beforeEnter: (to, from, next) => {
+        if (!isAuth()) {
+            next()
+        }
+        next({ name: 'dashboard' })
+    }
+},
+{
+    path: '/register',
+    component: () => { return import('./components/Register/Register.vue') },
+    name: 'register',
+    beforeEnter: (to, from, next) => {
+        if (!isAuth()) {
+            next()
+        }
+        next({ name: 'dashboard' })
+    }
+}
 ];
 
 const router = createRouter({
