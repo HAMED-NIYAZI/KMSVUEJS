4a79db1ec9deb8dd66958ebafc2cb6678289503a
hossein shirinegad <hosseinshirinegad.66@gmail.com>
2024-07-31 23:08:47 +0330

    fix bugs

--- src/components/Organization/OrganizationPage.vue
+++ src/components/Organization/OrganizationPage.vue
@@ -3,11 +3,7 @@
     <div class="my-auto">
       <div class="d-flex">
         <h4 class="content-title mb-0 my-auto">
-          <router-link
-            :to="{ name: 'organization' }"
-            class="content-title mb-0 my-auto"
-            >سازمان ها</router-link
-          >
+          <router-link :to="{ name: 'organization' }" class="content-title mb-0 my-auto">سازمان ها</router-link>
         </h4>
       </div>
     </div>
@@ -15,53 +11,32 @@
   <div class="row">
     <div class="col-lg-12">
       <div class="row pad">
-        <router-link
-          :to="{ name: 'create_organization' }"
-          class="btn btn-success btn-icon"
-          title="ایجاد سازمان"
-        >
+        <router-link :to="{ name: 'create_organization' }" class="btn btn-success btn-icon" title="ایجاد سازمان">
           <i class="fa fa-plus"></i>
         </router-link>
-        <router-link
-          v-if="OrganizationViewList_Value && OrganizationViewList_Value.id"
-          :to="{
+        <router-link v-if="OrganizationViewList_Value && OrganizationViewList_Value.id" :to="{
             name: 'edit_organization',
             params: { id: OrganizationViewList_Value.id },
-          }"
-          class="btn btn-warning btn-icon mr-2"
-          title="ویرایش سازمان"
-        >
+          }" class="btn btn-warning btn-icon mr-2" title="ویرایش سازمان">
           <i class="fa fa-pen"></i>
         </router-link>
-        <div
-          v-if="loadingRemove"
-          class="spinner-border text-primary"
-          role="status"
-        >
+        <div v-if="loadingRemove" class="spinner-border text-primary" role="status">
           <span class="sr-only"></span>
         </div>
         <template v-else>
-          <a
-            href="#"
-            @click="
-              remove(
-                OrganizationViewList_Value.id,
-                OrganizationViewList_Value.persianTitle
-              )
-            "
-            v-if="OrganizationViewList_Value && OrganizationViewList_Value.id"
-            class="btn btn-danger btn-icon mr-2"
-            title="حذف سازمان"
-          >
+          <a href="#" @click="
+            remove(
+              OrganizationViewList_Value.id,
+              OrganizationViewList_Value.persianTitle
+            )
+            " v-if="OrganizationViewList_Value && OrganizationViewList_Value.id" class="btn btn-danger btn-icon mr-2"
+            title="حذف سازمان">
             <i class="fa fa-trash"></i>
           </a>
         </template>
       </div>
-      <OrganizationTree
-        :key="componentKeyOrganizationTree"
-        @ReloadOrganizationEdit="FReloadOrganizationEdit"
-        :tree_name="tree_name"
-      />
+      <OrganizationTree :key="componentKeyOrganizationTree" @ReloadOrganizationEdit="FReloadOrganizationEdit"
+        :tree_name="tree_name" />
     </div>
   </div>
 </template>
@@ -88,42 +63,41 @@ let OrganizationViewList_Value = computed(() =>
   useLocalStorageService.getTreeSelectedItem(tree_name.value)
 );
 
-async function remove(id,name) {
-    Swal.fire({
-        title: "آیا مایل به حذف  (" + name + ")  هستید؟",
-        text: "آیتم حذف شده قابل بازیابی نمی باشد",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "بله، حذف کن!",
-        cancelButtonText: "انصراف"
-    }).then(async (result) => {
-        if (result.isConfirmed) {
-          loadingRemove.value = true;
-  try {
-    let response = await OrganizationService.delete(id);
+async function remove(id, name) {
+  Swal.fire({
+    title: "آیا مایل به حذف  (" + name + ")  هستید؟",
+    text: "آیتم حذف شده قابل بازیابی نمی باشد",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "بله، حذف کن!",
+    cancelButtonText: "انصراف"
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      loadingRemove.value = true;
+      try {
+        let response = await OrganizationService.delete(id);
 
-    if (
-      response.data.result == 4 ) {
-      toastService.error(response.data.message, { timeout: 2000 });
-      return;
-    }
-
-    if (response.data.result == 0) {
-      toastService.success('عملیات حذف با موفقیت انجام شد', { timeout: 2000 });
-     
-      useLocalStorageService.setTreeSelectedItem(tree_name.value, null);
-
-      FupdateKnowledgeFieldTree();
-    }
-  } catch (err) {
-    toastService.error(err.message, { timeout: 2000 });
-  } finally {
-    loadingRemove.value = false;
-  }
+        if (
+          response.data.result == 4) {
+          toastService.error(response.data.message, { timeout: 2000 });
+          return;
         }
-    });
+
+        if (response.data.result == 0) {
+          toastService.success('عملیات حذف با موفقیت انجام شد', { timeout: 2000 });
+
+          useLocalStorageService.setTreeSelectedItem(tree_name.value, null);
+          FupdateOrganizationTree()
+        }
+      } catch (err) {
+        toastService.error(err.message, { timeout: 2000 });
+      } finally {
+        loadingRemove.value = false;
+      }
+    }
+  });
 }
 </script>
 <style scoped>
